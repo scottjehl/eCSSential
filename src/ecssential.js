b346a23343dbe8b0b2fd580f586e42f696af7a93
Scott Jehl <scott@sj.(none)>
2012-06-06 19:29:27 -0400

    1) renamed IE bypass to oldIE and moved the use of that argument down further. 2) Removed any default dependency on server-side concatenation. Now by default, the script will create separate link elements for each stylesheet URL. The concat argument allows a passed function for parsing an array of urls and returning a string. That resulting string will be dropped into a single link element's href.

--- src/ecssential.js
+++ src/ecssential.js
@@ -3,21 +3,20 @@
 window.matchMedia=window.matchMedia||function(a,b){var c,d=a.documentElement,e=d.firstElementChild||d.firstChild,f=a.createElement("body"),g=a.createElement("div");return g.id="mq-test-1",g.style.cssText="position:absolute;top:-100em",f.style.background="none",f.appendChild(g),function(a){return g.innerHTML='&shy;<style media="'+a+'"> #mq-test-1 { width: 42px; }</style>',d.insertBefore(f,e),c=g.offsetWidth==42,d.removeChild(f),{matches:c,media:a}}}(document)
 
 /*! eCSSential.js: An experiment in optimized loading of mobile-first responsive CSS. [c]2012 @scottjehl, MIT/GPLv2 */
-window.eCSSential = function( css, okayIElte8 ){
+window.eCSSential = function( css, oldIE, concat ){
 	"use strict";
 	var load = [],
 		defer = [],
 		w = window,
-		d = w.document, 
-		freePass = okayIElte8 && /*@cc_on!@*/0 && w.navigator.appVersion.match( /MSIE [678]\./ ),
+		d = w.document,
 		insLoc = d.getElementsByTagName( "script" )[0];
-
+	
 	for( var mq in css ){					
 		if( css.hasOwnProperty( mq ) ){
 			// if stylesheet will apply at current viewport width, 
-			// or browser is lteIE8 and you're intending to polyfill mq support for it,
+			// or browser is IE lte version 8 and you're intending to polyfill media query support for it,
 			// queue for blocking load
-			if( freePass || w.matchMedia( mq ).matches ){
+			if( ( oldIE && /*!@cc_on!@*/0 && w.navigator.appVersion.match( /MSIE [678]\./ ) ) || w.matchMedia( mq ).matches ){
 				load.push( css[ mq ] );
 			}
 			// otherwise, queue it for deferred load, as long as it could potentially apply, by checking the same query against device-width
@@ -27,17 +26,32 @@ window.eCSSential = function( css, okayIElte8 ){
 		}
 	}
 	
+	// Make link elements (or one concat'd link) from an array of Stylesheets
+	// first argument is array of urls, second argument is bool for inserting meta element marker (only used in block)
+	function makeLinks( arr ){
+		var marker = arr === load ? '<meta id="eCSS">' : '',
+			start = '<link rel="stylesheet" href="',
+			end = '">';
+		// if the concat option is specified (recommended), pass the array through it and dump the resulting string into a single stylesheet url
+		if( concat ){
+			return '<link rel="stylesheet" href="' + o.concat( arr ) + '>' + marker;
+		}
+		// otherwise, make separate link elements
+		else {
+			return start + arr.join( end + start ) + end + marker;
+		}
+	}
+	
 	// document.write the stylesheet that should block
 	if( load.length ){
-		d.write( '<link rel="stylesheet" href="' + load.join( "," ) + '=concat"><meta id="eCSS">' );
+		d.write( makeLinks( load ) );
 		insLoc = d.getElementById( "eCSS" );
 	}
 	
 	// defer the load of the stylesheet that could later apply
 	if( defer.length ){
-		var link = d.createElement( "link" );
-		link.rel = "stylesheet";
-		link.href = defer.join( "," ) + "=concat";
-		insLoc.parentNode.insertBefore( link, insLoc );
+		var div = d.createElement( "div" );
+		div.innerHTML = makeLinks( defer );
+		insLoc.parentNode.insertBefore( div, insLoc );
 	}
 };
