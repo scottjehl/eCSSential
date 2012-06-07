8045feb94f380f7230d95633e864249f3f59aff5
Scott Jehl <scott@sj.local>
2012-06-07 09:58:14 -0400

    moved configuration to a hash argument. options are now set in that hash. currently available: oldIE, concat, and deferAll options. deferAll is new and allows you to prevent the default behavior of preventing width and height-based media queries from being requested if it appears they couldn't apply on the current device

--- src/ecssential.js
+++ src/ecssential.js
@@ -3,25 +3,35 @@
 window.matchMedia=window.matchMedia||function(a,b){var c,d=a.documentElement,e=d.firstElementChild||d.firstChild,f=a.createElement("body"),g=a.createElement("div");return g.id="mq-test-1",g.style.cssText="position:absolute;top:-100em",f.style.background="none",f.appendChild(g),function(a){return g.innerHTML='&shy;<style media="'+a+'"> #mq-test-1 { width: 42px; }</style>',d.insertBefore(f,e),c=g.offsetWidth==42,d.removeChild(f),{matches:c,media:a}}}(document)
 
 /*! eCSSential.js: An experiment in optimized loading of mobile-first responsive CSS. [c]2012 @scottjehl, MIT/GPLv2 */
-window.eCSSential = function( css, oldIE, concat ){
+window.eCSSential = function( css, config ){
 	"use strict";
 	var load = [],
 		defer = [],
+		// All options false or null by default; no need for a mixin
+		o = config || {},
 		w = window,
 		d = w.document,
-		insLoc = d.getElementsByTagName( "script" )[0];
+		insLoc = d.getElementsByTagName( "script" )[0],
+		re = /(min|max)-(width|height)/;
 	
 	for( var mq in css ){					
 		if( css.hasOwnProperty( mq ) ){
-			// if stylesheet will apply at current viewport width, 
-			// or browser is IE lte version 8 and you're intending to polyfill media query support for it,
-			// queue for blocking load
-			if( ( oldIE && /*!@cc_on!@*/0 && w.navigator.appVersion.match( /MSIE [678]\./ ) ) || w.matchMedia( mq ).matches ){
+			// if media query evaluates true,
+			// or if the non-default config.oldIE option is true and the browser is IE 6-8
+			// queue the stylesheet for a renderer-blocking load
+			if( ( o.oldIE && /*!@cc_on!@*/0 && w.navigator.appVersion.match( /MSIE [678]\./ ) ) || w.matchMedia( mq ).matches ){
 				load.push( css[ mq ] );
 			}
-			// otherwise, queue it for deferred load, as long as it could potentially apply, by checking the same query against device-width
-			else if( w.matchMedia( mq.replace( /(min|max)(\-width)/, "$1-device$2" ) ).matches ) {
-				defer.push( css[ mq ] );
+			// otherwise, decide whether to queue it for deferred load
+			else {
+				// Queue for deferred load some stylesheets that didn't evaluate true the first time
+				// Note: this means many stylesheets intended for conditions that could never apply (such as a width wider than the maximum device width) will be loaded anyway, causing more HTTP requests.
+				// min/max-width/height queries are by default evaluated to see if they could never apply on the current screen
+				// by running them as a "device" query instead of a viewport query. 
+				// You can disable this behavior and defer every stylesheet by setting the deferAll configuration option
+				if( o.deferAll || !mq.match( re ) || w.matchMedia( mq.replace( re, "$1-device-$2" ) ).matches ){
+					defer.push( css[ mq ] );
+				}
 			}
 		}
 	}
@@ -33,7 +43,7 @@ window.eCSSential = function( css, oldIE, concat ){
 			start = '<link rel="stylesheet" href="',
 			end = '">';
 		// if the concat option is specified (recommended), pass the array through it and dump the resulting string into a single stylesheet url
-		if( concat ){
+		if( o.concat ){
 			return '<link rel="stylesheet" href="' + o.concat( arr ) + '>' + marker;
 		}
 		// otherwise, make separate link elements
